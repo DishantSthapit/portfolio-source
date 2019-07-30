d282846e52b512b7253be64bf037b7f8739128e0
Dishant Sthapit <dishant.sthapit@bigtincan.com>
2019-07-30 19:36:02 +1000

    Adding Servie data as JSON file to fetch from

---
+++ src/json/serviceData.js
@@ -0,0 +1,26 @@
+
+const servicesData = [
+    {
+        id:1,
+        image:require('./../img/responsive.png'),
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:2,
+        image:require('./../img/seo.png'),
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:3,
+        image:require('./../img/testing.png'),
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    },
+    {
+        id:4,
+        image:require('./../img/webdesign.png'),
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+    }
+
+]
+
+export default servicesData;
