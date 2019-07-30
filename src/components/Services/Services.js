8a5dc7996e37fbca8ecae21b91cb2cb946d52f00
Dishant Sthapit <dishant.sthapit@bigtincan.com>
2019-07-30 19:42:58 +1000

    Creating new Service component

---
+++ src/components/Services/Services.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./index.css";
+import serviceData from "./../../json/serviceData"
+class Services extends React.Component {yarn 
+  constructor(props) {
+      super(props);
+      this.state = {
+      };
+	}
+
+  render() {     
+    return(
+        <div className="services-overview-wrapper">
+            <div className="section-heading">SERVICES</div>
+            <div className="services-content-wrapper">
+                {serviceData.map(item => {
+                    return <div className="service-item-wrapper" key={item.key}>
+                                <div className="service-image-wrapper">
+                                    <img src={item.image} className="service-image"></img>
+                                    <div className="service-description">{item.description}</div>
+                                </div>
+                            </div>
+
+                })}
+            </div>
+        </div>
+    );
+  }
+}
+
+export default Services;
